2cfcbce6db272dd5e99de5b5cabc1f3a134df627
kodjoSoule <souleymanekodjo@gmail.com>
2024-01-01 18:47:52 +0000

    added directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { TestComponent } from './test/test.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NgFor, TestComponent, NotesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
 })
 export class AppComponent {
-  title = 'project-1-notes';
-  name = 'Kodjo Souleymane';
-  inputVal = 'Kodjo Souleymane';
-  imageLink = 'https://picsum.photos/200/300';
-  inputName = 'attribute binding';
-  isHighlighted = true;
+
 }
