0550d040feb965c431dca6b7da6881d5c1e1e2b6
kodjoSoule <souleymanekodjo@gmail.com>
2024-01-01 16:06:04 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
   title = 'project-1-notes';
+  name = 'Kodjo Souleymane';
+  inputVal = 'Kodjo Souleymane';
+  imageLink = 'https://picsum.photos/200/300';
+  inputName = 'attribute binding';
+  isHighlighted = true;
 }
